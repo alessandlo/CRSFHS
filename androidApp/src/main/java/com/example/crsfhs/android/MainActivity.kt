c3b0a9bd97e79137a4ec4499e9d484eac8d58ad3
Alessandro <96017762+alessandlo@users.noreply.github.com>
2022-05-16 12:52:22 +0200

    Initial commit

---
+++ androidApp/src/main/java/com/example/crsfhs/android/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.crsfhs.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.crsfhs.Greeting
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CRSFHS"
+include(":androidApp")
+include(":shared")
