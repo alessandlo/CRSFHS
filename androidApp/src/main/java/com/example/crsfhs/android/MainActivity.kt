7f394c8d76d3910a16ae707d7e17e950112b9d9c
Alessandro <96017762+alessandlo@users.noreply.github.com>
2022-05-20 18:13:20 +0200

    Version 0.0.1 uploaded

--- androidApp/src/main/java/com/example/crsfhs/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/crsfhs/android/MainActivity.kt
@@ -2,13 +2,22 @@ package com.example.crsfhs.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.crsfhs.Greeting
-import android.widget.TextView
+import android.view.View
+import androidx.core.view.WindowCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val s: Array<String> = Array(5){i -> "Test $i"}
+
+        val recyclerView = findViewById<RecyclerView>(R.id.rv)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val adapter = CustomAdapter(s)
+        recyclerView.adapter = adapter
     }
 }
