315483e0cde84937824ded1faa407d27c0ff11eb
W0JW0J <95754666+W0JW0J@users.noreply.github.com>
2022-06-27 15:59:14 +0200

    Version 0.0.17
    Logo eingebaut, Version vor nav_graph_pre

--- androidApp/src/main/java/com/example/crsfhs/android/activities/FirstTimeMainActivity.kt
+++ androidApp/src/main/java/com/example/crsfhs/android/activities/FirstTimeMainActivity.kt
@@ -7,8 +7,8 @@ import com.example.crsfhs.android.R
 class FirstTimeMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.fragment_pre_willkommensseite)
-        setContentView(R.layout.fragment_pre_wie_funktioniert_das)
+        setContentView(R.layout.fragment_pre_willkommensseite)
+        //setContentView(R.layout.fragment_pre_wie_funktioniert_das)
         //setContentView(R.layout.fragment_pre_datenschutz)
     }
 }
