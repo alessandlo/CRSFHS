efde051736b9d6fab5a7ee32bd411fcab28f2043
Alessandro <96017762+alessandlo@users.noreply.github.com>
2022-06-29 18:00:37 +0200

    Version 0.0.24 uploaded

--- androidApp/src/main/java/com/example/crsfhs/android/activities/CustomAdapter.kt
+++ androidApp/src/main/java/com/example/crsfhs/android/activities/CustomAdapter.kt
@@ -1,19 +1,39 @@
 package com.example.crsfhs.android.activities
 
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
 import com.example.crsfhs.android.R
+import com.example.crsfhs.android.api.HairdresserDetails
 
-class CustomAdapter(private val dataSet: Array<String>) : RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+class CustomAdapter(private val dataSet: ArrayList<HairdresserDetails>) : RecyclerView.Adapter<CustomAdapter.ViewHolder>(), Filterable {
+    private val dataSetC: ArrayList<HairdresserDetails> = ArrayList<HairdresserDetails>()
+
+    init {
+        dataSetC.addAll(dataSet)
+    }
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
-        val textView: TextView
+        val hairdresser_location: TextView
+        val hairdresser_name: TextView
+        val hairdresser_icon: ImageView
 
         init {
-            textView = view.findViewById(R.id.textView)
+            hairdresser_location = view.findViewById(R.id.hairdresser_location)
+            hairdresser_name = view.findViewById(R.id.hairdresser_name)
+            hairdresser_icon = view.findViewById(R.id.hairdresser_icon)
+
+            hairdresser_icon.setImageResource(R.drawable.test)
+            hairdresser_icon.setBackgroundResource(R.drawable.rounded_corners_image)
+            hairdresser_icon.scaleType = ImageView.ScaleType.CENTER_CROP
+            hairdresser_icon.clipToOutline = true
         }
     }
 
@@ -24,10 +44,45 @@ class CustomAdapter(private val dataSet: Array<String>) : RecyclerView.Adapter<C
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.textView.text = dataSet[position]
+        holder.hairdresser_name.text = dataSetC[position].name
+        holder.hairdresser_location.text = dataSetC[position].address.city
+        holder.hairdresser_icon.load(dataSetC[position].img.icon)
     }
 
     override fun getItemCount(): Int {
-        return dataSet.size
+        return dataSetC.size
+    }
+
+    fun addItems(array: ArrayList<HairdresserDetails>){
+        dataSet.addAll(array)
+        notifyDataSetChanged()
+    }
+
+    override fun getFilter(): Filter {
+        return object : Filter(){
+            override fun performFiltering(p0: CharSequence?): FilterResults {
+                val string = p0?.toString()?.lowercase() ?: ""
+                val filteredList = ArrayList<HairdresserDetails>()
+
+                if(string.isEmpty()){
+                    filteredList.addAll(dataSet)
+                }else{
+                    dataSet.filter {
+                        it.name.lowercase().contains(p0!!.toString().lowercase())
+                    }.forEach { filteredList.add(it) }
+                }
+                return FilterResults().apply { values = filteredList}
+            }
+
+            override fun publishResults(p0: CharSequence?, p1: FilterResults?) {
+                if(p1?.values == null){
+                    dataSetC.clear()
+                }else{
+                    dataSetC.clear()
+                    dataSetC.addAll(p1.values as ArrayList<HairdresserDetails>)
+                }
+                notifyDataSetChanged()
+            }
+        }
     }
 }
--- androidApp/src/main/java/com/example/crsfhs/android/fragments/StartseiteFragment.kt
+++ androidApp/src/main/java/com/example/crsfhs/android/fragments/StartseiteFragment.kt
@@ -1,60 +1,74 @@
 package com.example.crsfhs.android.fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.crsfhs.android.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.crsfhs.android.activities.CustomAdapter
+import com.example.crsfhs.android.api.DbApi
+import com.example.crsfhs.android.api.HairdresserDetails
+import com.example.crsfhs.android.api.HairdresserList
+import com.example.crsfhs.android.databinding.FragmentStartseiteBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [StartseiteFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class StartseiteFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentStartseiteBinding
+    private lateinit var adapter: CustomAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_startseite, container, false)
+        binding = FragmentStartseiteBinding.inflate(inflater, container, false)
+
+        val hairdresser: ArrayList<HairdresserDetails> = getAllHairdressers()
+
+        val searchbar = binding.searchbar.searchbarText
+        searchbar.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(p0: Editable?) {}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                adapter.filter.filter(p0)
+            }
+        })
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment StartseiteFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            StartseiteFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun getAllHairdressers(): ArrayList<HairdresserDetails> {
+        val retrofitData = DbApi.retrofitService.getHairdressers()
+        val list: ArrayList<HairdresserDetails> = ArrayList()
+
+        retrofitData.enqueue(object : Callback<HairdresserList> {
+            override fun onResponse(
+                call: Call<HairdresserList>,
+                response: Response<HairdresserList>
+            ) {
+                println(response.message())
+                response.body()!!.items.forEach {
+                    list.add(it)
+                }
+                list.let {
+                    val recyclerView = binding.rv
+                    recyclerView.layoutManager = LinearLayoutManager(activity)
+
+                    adapter = CustomAdapter(it)
+
+                    recyclerView.adapter = adapter
                 }
             }
+
+            override fun onFailure(call: Call<HairdresserList>, t: Throwable) {
+                Log.e("Hairdresser", "onFailure: " + t.message)
+            }
+        })
+        return list
     }
 }
