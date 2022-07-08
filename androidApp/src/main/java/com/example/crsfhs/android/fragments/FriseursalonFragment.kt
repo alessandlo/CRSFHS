9830a9b410759b67249962bea0c55f63893904d5
W0JW0J <95754666+W0JW0J@users.noreply.github.com>
2022-07-08 10:01:13 +0200

    Version 0.0.32
    Sharebutton + Präsiseite

--- androidApp/src/main/java/com/example/crsfhs/android/fragments/FriseursalonFragment.kt
+++ androidApp/src/main/java/com/example/crsfhs/android/fragments/FriseursalonFragment.kt
@@ -1,60 +1,62 @@
 package com.example.crsfhs.android.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.text.Html
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.core.text.HtmlCompat
 import com.example.crsfhs.android.R
+import com.example.crsfhs.android.databinding.FragmentFriseursalonBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FriseursalonFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FriseursalonFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var binding: FragmentFriseursalonBinding
+    private lateinit var btn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
+        //btn = findViewById(R.id.sharebtn)
+
+
+
+
         }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentFriseursalonBinding.inflate(inflater, container, false)
+
+
+
+        binding.sharebtn.setOnClickListener{
+            // Text rausholen
+            val s = binding.hairsalonName.text.toString()
+
+            // Share Intent
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_SUBJECT, "Empfehlung geht raus!")
+                putExtra(Intent.EXTRA_TEXT, s)
+                putExtra(Intent.EXTRA_STREAM,Uri.parse("/drawable/fiverr_reshairvation_r1_02_final.png"))
+                type = "*/*"
+            }
+
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
+
+        }
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friseursalon, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FriseursalonFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FriseursalonFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
